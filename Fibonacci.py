02056d3c7f8c97dc7073bba98e88dc96e4528cbc
Ekuuin <71789461+Ekuuin@users.noreply.github.com>
2021-06-09 19:13:49 -0500

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -3,8 +3,7 @@ import numpy as np
 def Fibonacci(n):
     if n < 2:
         return 1
-    else:
-        return Fibonacci(n-1) + Fibonacci(n-2)
+    return Fibonacci(n-1) + Fibonacci(n-2)
 
 rango = np.arange(0 , 35)
 valores = np.array(rango)
